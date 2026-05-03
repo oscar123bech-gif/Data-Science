2b381d6b448e2b2b0183b814a04f9d23c2671703
oscar123bech-gif <oscar123bech@gmail.com>
2026-05-03 17:58:20 +0100

    Add files via upload

--- stepsinml.py
+++ stepsinml.py
@@ -1,9 +1,25 @@
 import numpy as np
 import pandas as pd
 data = pd.read_csv("Data.csv")
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
 
 x = data.iloc[ : ,0:3]
 y = data.iloc[ : ,3]
 
+
+
+imputer = SimpleImputer(missing_values= np.nan,strategy= "mean" )
+
+
+
+imputer.fit (x.iloc[ : ,1:3])
+x.iloc[ : ,1:3] = imputer.transform(x.iloc[ : ,1:3])
 print (x)
 print(y)
+
+firstcollum = ColumnTransformer(transformers=[("encoder",OneHotEncoder(),[0])])
+x = np.array(firstcollum.fit_transform(x))
+
+print(x)
