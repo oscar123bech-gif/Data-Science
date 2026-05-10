3ebbf3d3642e0d1d925dee86f8ccc5aed5112855
oscar123bech-gif <oscar123bech@gmail.com>
2026-05-10 17:54:02 +0100

    Add files via upload

--- stepsinml.py
+++ stepsinml.py
@@ -4,6 +4,9 @@ data = pd.read_csv("Data.csv")
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 x = data.iloc[ : ,0:3]
 y = data.iloc[ : ,3]
@@ -22,4 +25,17 @@ print(y)
 firstcollum = ColumnTransformer(transformers=[("encoder",OneHotEncoder(),[0])])
 x = np.array(firstcollum.fit_transform(x))
 
+
+
 print(x)
+
+labelencoder = LabelEncoder()
+y = labelencoder.fit_transform(y)
+print(y)
+xtrain,xtest,ytrain,ytest = train_test_split(x,y,test_size=0.2,random_state=1)
+
+standard = StandardScaler()
+xtrain = standard.fit_transform(xtrain)
+xtest = standard.fit_transform(xtest)
+
+print(xtrain,xtest)
